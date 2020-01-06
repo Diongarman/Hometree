fed797eb403715c82a1d6604f8d2aa2f5307d9b1
Dion Garman <diongarman@gmail.com>
2020-01-06 22:42:29 +0000

    functionality: add listeners to table cells

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('hello wor');
+console.log('hello world');
+var App = /** @class */ (function () {
+    function App() {
+        this.init = (function bar() {
+            var elements = document.querySelectorAll("td");
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].addEventListener("click", function () {
+                    console.log("ricked");
+                });
+            }
+        }());
+    }
+    return App;
+}());
+;
+new App();
--- ts/app.ts
+++ ts/app.ts
@@ -1,1 +1,14 @@
-console.log('hello wor')
+console.log('hello world');
+
+class App {
+    init: void = (function bar(){
+        var elements = document.querySelectorAll<HTMLElement>("td");
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].addEventListener("click", function() {
+                console.log("ricked");
+        });
+}
+    }());
+};
+
+new App();
