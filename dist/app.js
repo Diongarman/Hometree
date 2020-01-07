068cd11464325b0309c956d69b7d1a1575c72997
Dion Garman <diongarman@gmail.com>
2020-01-07 22:30:51 +0000

    functionality: added tick functionality (only one table cell atm)

--- dist/app.js
+++ dist/app.js
@@ -2,16 +2,37 @@
 console.log('hello world');
 var App = /** @class */ (function () {
     function App() {
-        this.onTableClick = function (event) {
-            console.log("rick sticked");
-            console.log(event);
-        };
+        var _this = this;
+        this.tableToggle = false;
         var elements = document.querySelectorAll("td");
+        console.log(elements);
         for (var i = 0; i < elements.length; i++) {
-            elements[i].addEventListener("click", this.onTableClick);
+            elements[i].addEventListener("click", function (e) { return _this.onTableClick(e); });
         }
     }
+    App.prototype.onTableClick = function (e) {
+        this.tableToggle = !this.tableToggle;
+        var td = e.target;
+        var img = td.lastElementChild;
+        if (this.tableToggle) {
+            td.classList.add('selected');
+            img.src = "/assets/tick.svg";
+        }
+        else {
+            td.classList.remove('selected');
+            img.src = "";
+        }
+    };
+    App.prototype.handleChange = function (event) {
+        console.log(event.style.set);
+    };
     return App;
 }());
 ;
 new App();
+//resources
+//https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/setProperty
+//https://www.w3schools.com/jsref/obj_cssstyledeclaration.asp
+//https://www.w3schools.com/howto/howto_js_toggle_class.asp
+//https://stackoverflow.com/questions/49226309/what-are-the-proper-typescript-types-for-addeventlistener-mousemove-and-its-eve
+//https://www.typescriptlang.org/docs/handbook/classes.html
--- ts/app.ts
+++ ts/app.ts
@@ -2,19 +2,48 @@ console.log('hello world');
 
 class App {
 
-
+    tableToggle: boolean = false;
     constructor() {
-        var elements = document.querySelectorAll<HTMLElement>("td");
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].addEventListener("click", this.onTableClick);
+        let elements = document.querySelectorAll<HTMLElement>("td");
+        console.log(elements)
+        for (let i = 0; i < elements.length; i++) {
+            
+            elements[i].addEventListener("click", (e: MouseEvent) => this.onTableClick(e));
         }
+
+        
+    }
+    private onTableClick(e: Event):void {
+        this.tableToggle = !this.tableToggle;
+        let td = (<Element>e.target)
+        let img =td.lastElementChild as HTMLImageElement
+
+        if (this.tableToggle) {
+
+            td.classList.add('selected');
+            img.src = "/assets/tick.svg";
+
+        } else {
+            
+            td.classList.remove('selected');
+            img.src = "";
+
+        }
+
     }
 
-    private onTableClick: { (event: MouseEvent): void } = (event: MouseEvent) => {
-        console.log("rick sticked");
-        console.log(event);
+
+    private handleChange(event:any):void {
+        console.log(event.style.set);
     }
 
 };
 
 new App();
+
+//resources
+//https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/setProperty
+//https://www.w3schools.com/jsref/obj_cssstyledeclaration.asp
+//https://www.w3schools.com/howto/howto_js_toggle_class.asp
+//https://stackoverflow.com/questions/49226309/what-are-the-proper-typescript-types-for-addeventlistener-mousemove-and-its-eve
+//https://www.typescriptlang.org/docs/handbook/classes.html
